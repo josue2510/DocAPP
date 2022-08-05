65e50869a2ed90fe2567a8ec43e4d6574d5fd402
froilan zambrana <froilanzambrana_@hotmail.com>
2022-08-05 12:23:45 -0400

    Almost Done

---
+++ Proyecto/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndescargar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnombres = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdocumento = new System.Windows.Forms.TextBox();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboDevices = new System.Windows.Forms.ComboBox();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.btnStartCamera = new System.Windows.Forms.Button();
+            this.btnTakePhoto = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btndescargar
+            // 
+            this.btndescargar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btndescargar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btndescargar.ForeColor = System.Drawing.Color.White;
+            this.btndescargar.Location = new System.Drawing.Point(843, 549);
+            this.btndescargar.Name = "btndescargar";
+            this.btndescargar.Size = new System.Drawing.Size(131, 27);
+            this.btndescargar.TabIndex = 0;
+            this.btndescargar.Text = "Terminar y Guardar";
+            this.btndescargar.UseVisualStyleBackColor = false;
+            this.btndescargar.Click += new System.EventHandler(this.btndescargar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 87;
+            this.label1.Text = "Nombres:";
+            // 
+            // txtnombres
+            // 
+            this.txtnombres.Location = new System.Drawing.Point(313, 23);
+            this.txtnombres.Name = "txtnombres";
+            this.txtnombres.Size = new System.Drawing.Size(144, 20);
+            this.txtnombres.TabIndex = 88;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtdocumento);
+            this.groupBox1.Controls.Add(this.txtnombres);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 58);
+            this.groupBox1.TabIndex = 89;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detalle Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 89;
+            this.label2.Text = "Nro. Documento:";
+            // 
+            // txtdocumento
+            // 
+            this.txtdocumento.Location = new System.Drawing.Point(100, 22);
+            this.txtdocumento.Name = "txtdocumento";
+            this.txtdocumento.Size = new System.Drawing.Size(144, 20);
+            this.txtdocumento.TabIndex = 88;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(79, 26);
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(139, 20);
+            this.txtdescripcion.TabIndex = 90;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtprecio);
+            this.groupBox2.Controls.Add(this.txtcantidad);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtdescripcion);
+            this.groupBox2.Location = new System.Drawing.Point(496, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 59);
+            this.groupBox2.TabIndex = 91;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Otros";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 93;
+            this.label5.Text = "Peso:";
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(274, 26);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(59, 20);
+            this.txtprecio.TabIndex = 92;
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(398, 26);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(59, 20);
+            this.txtcantidad.TabIndex = 92;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(355, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 91;
+            this.label4.Text = "Altura:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Descripcion:";
+            // 
+            // cboDevices
+            // 
+            this.cboDevices.FormattingEnabled = true;
+            this.cboDevices.Location = new System.Drawing.Point(13, 76);
+            this.cboDevices.Name = "cboDevices";
+            this.cboDevices.Size = new System.Drawing.Size(260, 21);
+            this.cboDevices.TabIndex = 93;
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(12, 103);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(478, 414);
+            this.picBox1.TabIndex = 94;
+            this.picBox1.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.Location = new System.Drawing.Point(496, 103);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(478, 414);
+            this.picBox2.TabIndex = 95;
+            this.picBox2.TabStop = false;
+            // 
+            // btnStartCamera
+            // 
+            this.btnStartCamera.Location = new System.Drawing.Point(279, 76);
+            this.btnStartCamera.Name = "btnStartCamera";
+            this.btnStartCamera.Size = new System.Drawing.Size(69, 21);
+            this.btnStartCamera.TabIndex = 96;
+            this.btnStartCamera.Text = "Comenzar";
+            this.btnStartCamera.UseVisualStyleBackColor = true;
+            this.btnStartCamera.Click += new System.EventHandler(this.btnStartCamera_Click);
+            // 
+            // btnTakePhoto
+            // 
+            this.btnTakePhoto.Location = new System.Drawing.Point(679, 77);
+            this.btnTakePhoto.Name = "btnTakePhoto";
+            this.btnTakePhoto.Size = new System.Drawing.Size(69, 21);
+            this.btnTakePhoto.TabIndex = 97;
+            this.btnTakePhoto.Text = "Capturar";
+            this.btnTakePhoto.UseVisualStyleBackColor = true;
+            this.btnTakePhoto.Click += new System.EventHandler(this.btnTakePhoto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(988, 588);
+            this.Controls.Add(this.btnTakePhoto);
+            this.Controls.Add(this.btnStartCamera);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.cboDevices);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btndescargar);
+            this.Name = "Form1";
+            this.Text = "PicDiagnostic";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndescargar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnombres;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdocumento;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboDevices;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.Button btnStartCamera;
+        private System.Windows.Forms.Button btnTakePhoto;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Proyecto/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using iTextSharp.text;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using AForge.Video.DirectShow;
+using AForge.Video;
+
+namespace Proyecto
+{
+    public partial class Form1 : Form
+    {
+        private bool ExistDevices;
+        private FilterInfoCollection MyDevices;
+        private VideoCaptureDevice MyWebCam;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndescargar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = string.Format("{0}.pdf", DateTime.Now.ToString("ddMMyyyyHHmmss"));
+
+            string PaginaHTML_Texto = Properties.Resources.Plantilla.ToString();
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@NOMBRE", txtnombres.Text);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@DOCUMENTO", txtdocumento.Text);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@CELULAR", txtdescripcion.Text);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FECHA", DateTime.Now.ToString("dd/MM/yyyy"));
+           
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream stream = new FileStream(savefile.FileName, FileMode.Create))
+                {
+                    Document pdfDoc = new Document(PageSize.A4,25,25,25,25);
+
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+                    pdfDoc.Add(new Phrase(""));
+
+                    iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(picBox2.Image, System.Drawing.Imaging.ImageFormat.Png);
+                    img.ScaleToFit(550, 500);
+                    img.Alignment = iTextSharp.text.Image.UNDERLYING;
+
+                    img.SetAbsolutePosition(pdfDoc.LeftMargin, pdfDoc.Bottom + 100);
+                    pdfDoc.Add(img);
+
+                    iTextSharp.text.Image img2 = iTextSharp.text.Image.GetInstance(Properties.Resources.marca, System.Drawing.Imaging.ImageFormat.Png);
+                    img2.ScaleToFit(60, 60);
+                    img2.Alignment = iTextSharp.text.Image.UNDERLYING;
+
+                    img2.SetAbsolutePosition(pdfDoc.LeftMargin, pdfDoc.Top - 60);
+                    pdfDoc.Add(img2);
+
+                    using (StringReader sr = new StringReader(PaginaHTML_Texto))
+                    {
+                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    }
+
+                    pdfDoc.Close();
+                    stream.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDevices();
+            btnTakePhoto.Visible = false;
+            btndescargar.Visible = false;
+        }
+
+        public void LoadDevices()
+        {
+            MyDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MyDevices.Count > 0)
+            {
+                ExistDevices = true;
+                for (int i = 0; i < MyDevices.Count; i++)
+                    cboDevices.Items.Add(MyDevices[i].Name.ToString());
+                cboDevices.Text = MyDevices[0].Name.ToString();
+            }
+            else
+                ExistDevices = false;
+        }
+
+        private void CloseWebCam()
+        {
+            if (MyWebCam != null && MyWebCam.IsRunning)
+            {
+                MyWebCam.SignalToStop();
+                MyWebCam = null;
+            }
+        }
+        private void Capture(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            picBox1.Image = image;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseWebCam();
+        }
+
+        private void btnStartCamera_Click(object sender, EventArgs e)
+        {
+            if (ExistDevices)
+            {
+                CloseWebCam();
+                int i = cboDevices.SelectedIndex;
+                string deviceName = MyDevices[i].MonikerString;
+                MyWebCam = new VideoCaptureDevice(deviceName);
+                MyWebCam.NewFrame += new NewFrameEventHandler(Capture);
+                MyWebCam.Start();
+                btnTakePhoto.Visible = true;
+            }
+        }
+
+        private void btnTakePhoto_Click(object sender, EventArgs e)
+        {
+            if (MyWebCam != null && MyWebCam.IsRunning)
+            {
+                picBox2.Image = picBox1.Image;
+                btndescargar.Visible = true;
+            }
+        }
+    }
+}
